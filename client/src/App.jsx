923cce73a0d28ec719a05f91e23d963e0318d224
dangeliki <angeliki.donta@outlook.com>
2025-08-14 00:08:56 +0300

    feat: create pages routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes , Route } from "react-router-dom"
+import Home from "./pages/home";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
+import Profile from "./pages/profile";
+import About from "./pages/about";
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>
-      App
-    </h1>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/sign-in" element={<Signin />}/>
+    <Route path="/sign-up" element={<Signup />}/>
+    <Route path="/about" element={<About />}/>
+    <Route path="/profile" element={<Profile />}/>
+  </Routes>
+  </BrowserRouter>
   )
 }
