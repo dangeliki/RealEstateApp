77040442b39af0cba0f867df5ab0366c1611d359
dangeliki <angeliki.donta@outlook.com>
2025-08-16 19:39:09 +0300

    create middleware and error function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/users.js'
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup= async (req,res) => {
+export const signup= async (req,res,next) => {
     const {username,email,password} = req.body;
     // Encrypt password
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -11,7 +12,7 @@ export const signup= async (req,res) => {
         await newUser.save();
         res.status(201).json("User created successfully");
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 
 };
--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/users.route.js';
 import authRouter from './routes/auth.js';
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 // Σύνδεση με την βάση 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -25,3 +25,13 @@ app.listen(3000, () => {
 
 app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter)
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
