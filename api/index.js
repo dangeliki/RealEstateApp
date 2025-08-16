9cdaaab2baa6690a4fceabb380dbe5b0114bef17
dangeliki <angeliki.donta@outlook.com>
2025-08-16 09:16:53 +0300

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose, { connect } from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/users.route.js';
+import authRouter from './routes/auth.js';
 
 dotenv.config();
 
@@ -14,9 +15,13 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+// Για να μπορώ να στειλω json requests
+app.use(express.json());
+
 // Callback για να ακουσει την πορτα 3000
 app.listen(3000, () => {
     console.log ('Server is running on port 3000');
 });
 
 app.use("/api/user",userRouter)
+app.use("/api/auth",authRouter)
