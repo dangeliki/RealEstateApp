ad93feae52d1e2452245e9ae088a15055259e1e4
dangeliki <angeliki.donta@outlook.com>
2025-09-08 21:05:49 +0300

    header search functionality

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import {FaSearch} from 'react-icons/fa'
 import { useSelector } from 'react-redux'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate, useLocation} from 'react-router-dom'
 
 export default function Header() {
+
     const {currentUser} = useSelector(state => state.user)
+    const [search,setSearch] = useState('');
+    const navigate = useNavigate();
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('search',search);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchFromUrl = urlParams.get('search');
+        if(searchFromUrl) {
+            setSearch(searchFromUrl);
+        }
+    },[location.search]);
+
   return (
     <header className='bg-gray-300 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -23,9 +44,14 @@ export default function Header() {
             </Link>
 
             {/* Search bar */}
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input type="text" placeholder='Search' className='bg-transparent focus:outline-0 w-24 sm:w-64' />
-                <FaSearch className='text-black'/>
+            <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+
+                <input type="text" placeholder='Search' className='bg-transparent focus:outline-0 w-24 sm:w-64' value={search} onChange={(e) => setSearch(e.target.value)} />
+
+                <button>
+                    <FaSearch className='text-black'/>
+                </button>
+
             </form>
 
             {/* Menu */}
