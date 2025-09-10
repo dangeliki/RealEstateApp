59a3485f6deacc4ceda7a25a3bfc194f4a6d736c
dangeliki <angeliki.donta@outlook.com>
2025-09-10 22:26:41 +0300

    fix: home page on small screen

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -59,7 +59,7 @@ export default function Home() {
 
       {/* Αποτελέσματα εγγραφών */}
 
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+      <div className='p-7 md:min-h-screen'>
 
         {/* Πρόσφατα προς πώληση */}
         {saleListings && saleListings.length>0 && (
